448175682759ee8cba4cca1f58c4a560395fe321
dgmedvedev <dgmedvedev.it@gmail.com>
2025-11-27 11:39:53 +0400

    Initial commit

---
+++ src/main/kotlin/com/example/spring/security_jwt/SecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring.security_jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityJwtApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityJwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/spring/security_jwt/SecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring.security_jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityJwtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
